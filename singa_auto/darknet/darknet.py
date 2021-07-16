f3f89863d83cb8195d8228dc4626cb9d0e218399
nailixing <xingnaili14@gmail.com>
2021-07-16 17:17:53 +0800

    Remove duplicate in TfFeedForward.py

--- singa_auto/darknet/darknet.py
+++ singa_auto/darknet/darknet.py
@@ -49,8 +49,10 @@ class METADATA(Structure):
                 ("names", POINTER(c_char_p))]
 
 import os
-print(os.getcwd())
-lib = CDLL("./libdarknet_gpu_server.so", RTLD_GLOBAL)
+cc = os.getcwd()
+print(cc)
+print(os.listdir(cc))
+lib = CDLL("libdarknet_gpu_server.so", RTLD_GLOBAL)
 lib.network_width.argtypes = [c_void_p]
 lib.network_width.restype = c_int
 lib.network_height.argtypes = [c_void_p]
